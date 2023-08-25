d435f6a6840a005d23bdae386d31e59d8bbc1eaf
arvene <arvene241@gmail.com>
2023-08-25 21:15:10 +0800

    add utils

--- libs/utils.ts
+++ libs/utils.ts
@@ -1,6 +1,23 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Format milliseconds into MM:SS
+export const formatDuration = (millis: number) => {
+  const minutes = Math.floor(millis / 60000);
+  const seconds = ((millis % 60000) / 1000).toFixed(0);
+  return `${minutes}:${Number(seconds) < 10 ? '0' : ''}${seconds}`;
+};
+
+// Format milliseconds into X minutes and Y seconds
+export const formatDurationForHumans = (millis: number) => {
+  const minutes = Math.floor(millis / 60000);
+  const seconds = ((millis % 60000) / 1000).toFixed(0);
+  return `${minutes} Mins ${seconds} Secs`;
+};
+
+// Get year from YYYY-MM-DD
+export const getYear = (date: string) => date.split('-')[0];
