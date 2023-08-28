3467be242b8858151a11676da3a22fc8c36da8e8
arvene <arvene241@gmail.com>
2023-08-28 19:41:12 +0800

    add Trackitem component

---
+++ components/TrackItem.tsx
@@ -0,0 +1,52 @@
+import { TracksItems } from "@/types/types";
+import { FaInfoCircle } from "react-icons/fa";
+import Image from "next/image";
+import Link from "next/link";
+import { formatDuration } from "@/libs/utils";
+
+interface TrackItemProps {
+  item: TracksItems;
+}
+
+const TrackItem = ({ item }: TrackItemProps) => {
+  return (
+    <li key={item.id} className="w-full">
+      <Link
+        href={`/track/${item.id}`}
+        className="flex items-center gap-[20px] group"
+      >
+        <div className="relative w-[50px] h-[50px]">
+          <Image src={item.album.images[0].url} alt="Album Artwork" fill />
+          <div className="absolute inset-0 w-full h-full flex items-center justify-center opacity-0 group-hover:opacity-100 text-white bg-[rgba(0,0,0,0.5)]">
+            <FaInfoCircle className="text-[25px]" />
+          </div>
+        </div>
+
+        <div className="flex-1 flex justify-between gap-4 overflow-hidden">
+          {/* <p className="truncate">{item.name}</p> */}
+          <div className="overflow-hidden">
+            <p className="truncate">{item.name}</p>
+            <div className="truncate text-lightGrey text-sm">
+              {item.artists.map(({ name }, i) => (
+                <span key={i}>
+                  {name}
+                  {item.artists.length > 0 && i === item.artists.length - 1
+                    ? ""
+                    : ","}
+                  &nbsp;
+                </span>
+              ))}
+              &nbsp;&middot;&nbsp;&nbsp;
+              {item.album.name}
+            </div>
+          </div>
+          <span className="text-lightGrey text-sm">
+            {formatDuration(item.duration_ms)}
+          </span>
+        </div>
+      </Link>
+    </li>
+  );
+};
+
+export default TrackItem;
