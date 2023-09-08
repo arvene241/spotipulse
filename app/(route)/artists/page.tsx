5d1edec624e3042dec7c764429e40a3ae3fa1906
arvene <arvene241@gmail.com>
2023-09-09 00:16:05 +0800

    useCallback

--- app/(route)/artists/page.tsx
+++ app/(route)/artists/page.tsx
@@ -11,7 +11,7 @@ import { cn } from "@/libs/utils";
 import { TopArtists } from "@/types/types";
 import Image from "next/image";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { FaInfoCircle } from "react-icons/fa";
 
 const Artists = () => {
@@ -25,13 +25,13 @@ const Artists = () => {
   const { data: longArtists } = useGetTopArtistsLongQuery(accessToken);
 
   useEffect(() => {
-    setTopArtists(longArtists)
-  },[longArtists])
+    setTopArtists(longArtists);
+  }, [longArtists]);
 
-  const changeData = (query: string) => {
+  const changeData = useCallback((query: string) => {
     setActiveQuery(query);
 
-    switch (activeQuery) {
+    switch (query) {
       case "short":
         setTopArtists(shortArtists);
         break;
@@ -45,56 +45,63 @@ const Artists = () => {
         setTopArtists(longArtists);
         break;
     }
-  };
+  }, [shortArtists, mediumArtists, longArtists]);
 
   return (
     <>
       <header className="md:flex block justify-between items-center">
-        <h2 className="font-black text-2xl text-center md:text-start">Top Artists</h2>
+        <h2 className="font-black text-2xl text-center md:text-start">
+          Top Artists
+        </h2>
         <div className="flex gap-5 mt-[30px] md:mt-0 justify-around md:justify-center">
           <Button
             content="All Time"
             handleClick={() => changeData("long")}
             variant="link"
-            className={cn({"underline text-white": activeQuery === "long"})}
+            className={cn({ "underline text-white": activeQuery === "long" })}
           />
           <Button
             content="Last 6 Months"
             handleClick={() => changeData("medium")}
             variant="link"
-            className={cn({"underline text-white": activeQuery === "medium"})}
+            className={cn({ "underline text-white": activeQuery === "medium" })}
           />
           <Button
             content="Last 4 Weeks"
             handleClick={() => changeData("short")}
             variant="link"
-            className={cn({"underline text-white": activeQuery === "short"})}
+            className={cn({ "underline text-white": activeQuery === "short" })}
           />
         </div>
       </header>
-      <ul className="grid grid-cols-sm-grid sm:grid-cols-md-grid md:grid-cols-lg-grid gap-5 mt-[50px]">
-          {topArtists &&
-            topArtists.items.map((item) => (
-              <li
-                key={item.id}
+      {topArtists && (
+        <ul className="grid grid-cols-sm-grid sm:grid-cols-md-grid md:grid-cols-lg-grid gap-5 mt-[50px]">
+          {topArtists.items.map((item) => (
+            <li key={item.id}>
+              <Link
+                href={`/artist/${item.id}`}
+                className="flex flex-col items-center justify-center group"
               >
-                <Link
-                  href={`/artist/${item.id}`}
-                  className="flex flex-col items-center justify-center group"
-                >
-                  <div className="relative">
-                    <Image src={item.images[0].url} alt={item.name} width="300" height="300" className="w-[150px] h-[150px] md:w-[200px] md:h-[200px] rounded-full object-cover" />
-                    <div className="absolute inset-0 w-full h-full flex items-center justify-center opacity-0 group-hover:opacity-100 text-white bg-[rgba(0,0,0,0.5)] rounded-full">
-                      <FaInfoCircle className="text-[25px]" />
-                    </div>
+                <div className="relative">
+                  <Image
+                    src={item.images[0].url}
+                    alt={item.name}
+                    width="300"
+                    height="300"
+                    className="w-[150px] h-[150px] md:w-[200px] md:h-[200px] rounded-full object-cover"
+                  />
+                  <div className="absolute inset-0 w-full h-full flex items-center justify-center opacity-0 group-hover:opacity-100 text-white bg-[rgba(0,0,0,0.5)] rounded-full">
+                    <FaInfoCircle className="text-[25px]" />
                   </div>
-                  <p className="text-center underline-offset-4 group-hover:underline my-[20px]">
-                    {item.name}
-                  </p>
-                </Link>
-              </li>
-            ))}
+                </div>
+                <p className="text-center underline-offset-4 group-hover:underline my-[20px]">
+                  {item.name}
+                </p>
+              </Link>
+            </li>
+          ))}
         </ul>
+      )}
     </>
   );
 };
